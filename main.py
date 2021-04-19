cb91a921eea83a0e7b0263bc2fac01e601fb1cf5
Mads Ehrhorn <mae@venzo.com>
2021-04-19 18:00:31 +0200

    Random orbits button.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import plotly.graph_objects as go
 import streamlit as st
+from numpy.random import random
 
 st.set_page_config(layout="wide")
 from modules import create_globe, plot_satellite_orbit_plotly, set_camera, set_layout
-
-st.write("Hej lorte Bjørn!")
 
 
 def input_options(i):
@@ -13,14 +12,14 @@ def input_options(i):
         f"Eccentricity {i}",
         min_value=0.0,
         max_value=1.0,
-        value=0.00155,
+        value=round(0.3 * random() + 0.1, 2),
         step=0.1,
         format="%f",
     )
     out_dict["orbital_period"] = st.sidebar.number_input(
         f"Orbital period (minutes) {i}",
         min_value=0.0,
-        value=717.966,
+        value=round(2000 * random() + 200, 2),
         step=100.0,
         format="%f",
     )
@@ -28,7 +27,7 @@ def input_options(i):
         f"Inclination (degrees) {i}",
         min_value=0.0,
         max_value=180.0,
-        value=56.396,
+        value=round(150 * random() + 1, 2),
         step=1.0,
         format="%f",
     )
@@ -36,7 +35,7 @@ def input_options(i):
         f"RA ascending node (hours) {i}",
         min_value=0.0,
         max_value=24.0,
-        value=23.263,
+        value=round(20 * random() + 1, 2),
         step=1.0,
         format="%f",
     )
@@ -44,22 +43,27 @@ def input_options(i):
         f"Argument of perigee (degrees) {i}",
         min_value=0.0,
         max_value=180.0,
-        value=125.549,
+        value=round(80 * random() + 50, 2),
         step=1.0,
         format="%f",
     )
     return out_dict
 
 
-no_orbits = st.sidebar.select_slider(label="No. of orbits", options=[1, 2, 3])
-
-surface = create_globe()
-layout = set_layout()
-camera = set_camera()
+no_orbits = st.sidebar.select_slider(label="No. of orbits", options=[1, 2, 3], value=3)
 
 coordinate_list = []
 for number in range(no_orbits):
     coordinate_list.append(input_options(number + 1))
+
+if st.button("Create random orbit(s)"):
+    coordinate_list = []
+    for number in range(no_orbits):
+        coordinate_list.append(input_options(number + 1))
+
+surface = create_globe()
+layout = set_layout()
+camera = set_camera()
 
 plot_data = [surface]
 for number, coordinate_set in enumerate(coordinate_list):
